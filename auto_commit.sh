ee8e427a33f3e32b453083fcddefd21da7c4b6bb
gvrajesh07 <gvrajesh.07@gmail.com>
2025-11-02 17:10:01 +0800

    Auto commit: 2025-11-02 17:10:01

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/rajesh/k8s || exit
-
+git checkout k8s/practice
 # Add all changes
 git add .
 
@@ -8,4 +8,4 @@ git add .
 git commit -m "Auto commit: $(date '+%Y-%m-%d %H:%M:%S')"
 
 # Push to branch
-git push origin main
+git push -u origin k8s/practice
